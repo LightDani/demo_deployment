d2eafa0de9ef6ef82dc2c1617adc722de1c9d007
Dwi Cahya Nur Faizi <cahyafaizi@gmail.com>
2025-06-01 08:16:22 +0700

    fix: update model loading to use compile flag and add new model file

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 # model = tf.keras.models.load_model("model/model.h5", compile=False)
 
 # load model using .keras
-model = tf.keras.models.load_model("model/arabic_model.keras", compile=False)
+model = tf.keras.models.load_model("model/model.keras")
 
 arabic_letters = [
     "ا - Alif",
